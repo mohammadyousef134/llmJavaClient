fea6fba733a7f9b5fada20096e3fa2c9f2c49480
mohammad <mohammadwesam13@gmail.com>
2026-05-07 13:58:44 +0300

    Initial commit

---
+++ src/main/java/com/example/llmjavaclient/LlmJavaClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.llmjavaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmJavaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlmJavaClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/llmjavaclient/LlmJavaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.llmjavaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmJavaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
