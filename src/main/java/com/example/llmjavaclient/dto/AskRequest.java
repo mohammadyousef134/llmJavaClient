4ed469aa196037d0fb73056a6018241847fc5669
mohammad <mohammadwesam13@gmail.com>
2026-05-07 14:09:38 +0300

    add AskRequest and AskResponse DTOs

---
+++ src/main/java/com/example/llmjavaclient/dto/AskRequest.java
@@ -0,0 +1,10 @@
+package com.example.llmjavaclient.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AskRequest {
+    private String prompt;
+}
